a81fbda116668df77147b59c21fec90cc1194137
Anselmo Junior <anselmojuniorjj@gmail.com>
2018-10-02 11:44:03 -0300

    put e delete

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -2,7 +2,10 @@ package com.produtos.apirest.resources;
 
 import com.produtos.apirest.models.Produto;
 import com.produtos.apirest.repository.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,23 +13,40 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "API REST Produtos")
+@CrossOrigin(origins = "*")
 public class ProdutoResource {
 
     @Autowired
     ProdutoRepository produtoRepository;
 
     @GetMapping("/produtos")
+    @ApiOperation(value = "Retorna uma lista de produtos")
     public List<Produto> listaProdutos(){
         return produtoRepository.findAll();
     }
 
     @GetMapping("/produto/{id}")
+    @ApiOperation(value = "Retorna um único produto de acordo com Id")
     public Produto listaProdutoUnico(@PathVariable(value = "id") long id){
         return produtoRepository.findById(id);
     }
 
     @PostMapping("/produto")
+    @ApiOperation(value = "Salva um produto")
     public Produto salvaProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    @DeleteMapping("/produto")
+    @ApiOperation(value = "Deleta um produto")
+    public void deletaProduto(@RequestBody Produto produto){
+        produtoRepository.delete(produto);
+    }
+
+    @PutMapping("/produto")
+    @ApiOperation(value = "Atualiza, altera, informações de um produto")
+    public Produto atualizaProduto(@RequestBody Produto produto){
         return produtoRepository.save(produto);
     }
 }
